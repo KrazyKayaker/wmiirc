41a510a236d71acde6d85218c570e7eb584dbf60
Ryan Kraay <edgewiser@sourceforge.net>
2011-05-08 23:01:30 +0200

    we now using the proper config file

--- wmiirc.rb
+++ wmiirc.rb
@@ -5,6 +5,11 @@ require 'bundler'
 
 Dir.chdir(File.dirname(__FILE__)) { Bundler.setup }
 
+# Bundler seems to change your yaml processor to Psych -- which doesn't work that well...
+# http://stackoverflow.com/questions/4980877/rails-error-couldnt-parse-yaml
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
+
 $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'wmiirc/loader'
 Wmiirc::Loader.run
